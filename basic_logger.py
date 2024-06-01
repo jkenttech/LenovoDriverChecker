8f125025ef003918c5ca98d540abbd7c3045cdda
RMITJake <s3905550@student.rmit.edu.au>
2024-06-02 00:55:27 +1000

    added timestamps to logs

--- basic_logger.py
+++ basic_logger.py
@@ -1,10 +1,11 @@
 import datetime as dt
+datetime_format = "%y-%m-%d %H:%M:%S"
 
 def info(string):
-    print(f'{dt.datetime.now()} INF: {string}')
+    print(f'{dt.datetime.now().strftime(datetime_format)} INF: {string}')
 
 def error(string):
-    print(f'{dt.datetime.now()} ERR: {string}')
+    print(f'{dt.datetime.now().strftime(datetime_format)} ERR: {string}')
 
 def debug(string):
-    print(f'{dt.datetime.now()} DBG: {string}')
+    print(f'{dt.datetime.now().strftime(datetime_format)} DBG: {string}')
