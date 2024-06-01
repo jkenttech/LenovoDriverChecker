c7136fb30f9b3df92d0776371f5e2e1842700b62
RMITJake <s3905550@student.rmit.edu.au>
2024-06-01 16:54:36 +1000

    driver list is scraped and all driver versions are retreived

--- main.py
+++ main.py
@@ -2,19 +2,39 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 import requests
 import sys
+import re
 
 serial = "MP09RFK7"
-components = [ 'Audio', 'BIOS/UEFI']
+components = []
 
 browser = webdriver.Chrome()
 
-for component in components:
-    url = f'https://pcsupport.lenovo.com/au/en/products/{serial}/downloads/driver-list/component?name={component}'
+def get_versions():
+    for component in components:
+        url = f'https://pcsupport.lenovo.com/au/en/products/{serial}/downloads/driver-list/component?name={component}'
+        browser.get(url)
+        soup = BeautifulSoup(browser.page_source, 'html.parser')
+        file = open(f'driver_html/{component.replace("/","")}.html', 'w', encoding='utf-8')
+        file.write(str(soup.prettify))
+        for datarow in soup.find_all(class_="simple-table-dataRow"):
+            title = datarow.find(class_="table-body-item").find("span").text
+            version = datarow.find_all(class_="table-body-width-item")
+            csv = open(f'drivers.csv', 'a')
+            csv.write(f'{title};{version[0].text};{version[1].text};{version[2].text}\n')
+
+def get_driver_categories():
+    url = f'https://pcsupport.lenovo.com/au/en/products/{serial}/downloads/driver-list'
     browser.get(url)
     soup = BeautifulSoup(browser.page_source, 'html.parser')
-    file = open(f'{component.replace("/","")}.html', 'w', encoding='utf-8')
-    file.write(str(soup.prettify))
-    for datarow in soup.find_all(class_="simple-table-dataRow"):
-        title = datarow.find(class_="table-body-item").find("span").text
-        version = datarow.find_all(class_="table-body-width-item")
-        print(f'{title}\t{version[0].text}\t{version[1].text}\t{version[2].text}')
+    raw_categories = soup.find_all(class_="title-row")
+    category_list = []
+    for category in raw_categories:
+        category = re.sub(r'\s\(\d{0,}\)\s?$', '', category.text)
+        category = re.sub(r'\s', '%20', category)
+        category_list.append(category)
+    return category_list
+
+if __name__ == "__main__":
+    print("running on main")
+    components = get_driver_categories()
+    get_versions()
