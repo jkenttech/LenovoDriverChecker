c75374f77ac4a45a603e3f04ab10f2869c42fecb
RMITJake <s3905550@student.rmit.edu.au>
2024-06-01 15:27:48 +1000

    audio and bios versions scraping successfully

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 from bs4 import BeautifulSoup
+from selenium import webdriver
 import requests
 import sys
 
 serial = "MP09RFK7"
-components = [ 'Audio' , 'BIOS/UEFI']
+components = [ 'Audio', 'BIOS/UEFI']
+
+browser = webdriver.Chrome()
 
 for component in components:
     url = f'https://pcsupport.lenovo.com/au/en/products/{serial}/downloads/driver-list/component?name={component}'
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
-    file = open(f'{component}.html', 'w', encoding='utf-8')
+    browser.get(url)
+    soup = BeautifulSoup(browser.page_source, 'html.parser')
+    file = open(f'{component.replace("/","")}.html', 'w', encoding='utf-8')
     file.write(str(soup.prettify))
+    for datarow in soup.find_all(class_="simple-table-dataRow"):
+        title = datarow.find(class_="table-body-item").find("span").text
+        version = datarow.find_all(class_="table-body-width-item")
+        print(f'{title}\t{version[0].text}\t{version[1].text}\t{version[2].text}')
