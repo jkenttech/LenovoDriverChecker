1a25fa84460c793d893d6f45341c44ac5cf8f372
RMITJake <s3905550@student.rmit.edu.au>
2024-06-03 01:44:59 +1000

    added colours to debug prints

--- basic_logger.py
+++ basic_logger.py
@@ -1,11 +1,16 @@
 import datetime as dt
 datetime_format = "%y-%m-%d %H:%M:%S"
 
+white = '\033[0m'
+green = '\033[32m'
+blue = '\033[34m'
+red = '\033[31m'
+
 def info(string):
-    print(f'{dt.datetime.now().strftime(datetime_format)} INF: {string}')
+    print(f'{dt.datetime.now().strftime(datetime_format)} {green}INF{white}: {string}')
 
 def error(string):
-    print(f'{dt.datetime.now().strftime(datetime_format)} ERR: {string}')
+    print(f'{dt.datetime.now().strftime(datetime_format)} {red}ERR{white}: {string}')
 
 def debug(string):
-    print(f'{dt.datetime.now().strftime(datetime_format)} DBG: {string}')
+    print(f'{dt.datetime.now().strftime(datetime_format)} {blue}DBG{white}: {string}')
