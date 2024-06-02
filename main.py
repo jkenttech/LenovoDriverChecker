7252b790b7520d2a88f7588431ca32bc2dbd41d9
RMITJake <s3905550@student.rmit.edu.au>
2024-06-02 15:08:18 +1000

    driver links are now scraped

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 import basic_logger as log
 import sys
 import re
@@ -19,8 +20,8 @@ componentCount = 0
 url_base_path = f'https://pcsupport.lenovo.com/au/en/products/{serial}'
 driver_path = f'{url_base_path}/downloads/driver-list'
 driver_query_path = f'{driver_path}/component?name='
-file_base_path = f'{serial}_driver_html/'
-csv_path = f'{serial}_drivers.csv'
+file_base_path = f'driver_csv'
+csv_path = f'{file_base_path}/{serial}_drivers.csv'
 
 def get_html(url):
     log.info(f'Downloading html data from {url}')
@@ -36,23 +37,65 @@ def driver_query(component):
 
 def get_driver_versions(component):
     soup = get_html(driver_query(component))
-    file_path = f'{file_base_path}{component.replace("/","-").replace("%20", " ")}.html'
+    # file_path = f'{file_base_path}{component.replace("/","-").replace("%20", " ")}.html'
 
+    component = component.replace("%20", " ")
     try:
-        file = open(file_path, 'w', encoding='utf-8')
-        file.write(str(soup.prettify))
         for datarow in soup.find_all(class_="simple-table-dataRow"):
-            component = component.replace("%20", " ")
-            title = datarow.find(class_="table-body-item").find("span").text
-            version = datarow.find_all(class_="table-body-width-item")
+            title = get_title_from_datarow(component, datarow)
+            datarow = datarow.find_all(class_="table-body-width-item")
+            size = get_size_from_datarow(component, datarow)
+            version = get_version_from_datarow(component, datarow)
+            date = get_date_from_datarow(component, datarow)
+            link = get_link_from_datarow(component, datarow)
             csv = open(f'{csv_path}', 'a')
-            csv.write(f'{component};{title};{version[0].text};{version[1].text};{version[2].text}\n')
+            csv.write(f'{component};{title};{size};{version};{date};{link}\n')
+            log.debug(f'{component};{title};{size};{version};{date};{link}')
     except:
-        log.error(f'Unable to write to {file_path}')
+        log.error(f'Unable to get all driver information for {component}')
     else:
         global componentCount
         componentCount = componentCount + 1
         log.info(f'Captured component {componentCount}/{len(components)}')
+
+def get_title_from_datarow(component, datarow):
+    try:
+        return datarow.find(class_="table-body-item").find("span").text
+    except:
+        log.error(f'Unable to find {component} title')
+        raise Exception
+
+def get_size_from_datarow(component, datarow):
+    try:
+        return datarow[0].text
+    except:
+        log.error(f'Unable to find {component} size in {datarow}')
+        raise Exception
+
+def get_version_from_datarow(component, datarow):
+    try:
+        return datarow[1].text
+    except:
+        log.error(f'Unable to find {component} size in {datarow}')
+        raise Exception
+
+def get_date_from_datarow(component, datarow):
+    try:
+        return datarow[2].text
+    except:
+        log.error(f'Unable to find {component} date in {datarow}')
+        raise Exception
+
+def get_link_from_datarow(component, datarow):
+    try:
+        link = datarow[4]
+        link = str(link).split("href=\"")
+        link = link[1].split("\"")
+        return link[0]
+    except:
+        log.error(f'Unable to find {component} link in {datarow}')
+        log.error(f'Specific line {datarow[4]}')
+        raise Exception
 
 def get_driver_categories():
     log.info(f'Getting driver categories...')
@@ -67,7 +110,7 @@ def get_driver_categories():
 
 if __name__ == "__main__":
     if not os.path.exists(file_base_path):
-        log.info(f'Creating driver_html folder')
+        log.info(f'Creating driver_csv folder')
         os.makedirs(file_base_path)
 
     log.info(f'Downloading driver information for {serial}...')
